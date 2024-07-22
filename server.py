f32d59e0d37ca6f80a01fd02e60b30d704146d51
Alexoqool <154623493+Alexoqool@users.noreply.github.com>
2024-07-22 21:23:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,13 +12,57 @@ while True:
     print('Got connection from', addr)
     request = c.recv(1024)
     if request[0]==0:
-        keyboard.press("w")
+        keyboard.press("q")
     if request[0]==1:
-        keyboard.press("a")
+        keyboard.press("w")
     if request[0]==2:
-        keyboard.press("s")
+        keyboard.press("e")
     if request[0]==3:
+        keyboard.press("r")
+    if request[0]==4:
+        keyboard.press("t")
+    if request[0]==5:
+        keyboard.press("y")
+    if request[0]==6:
+        keyboard.press("u")
+    if request[0]==7:
+        keyboard.press("i")
+    if request[0]==8:
+        keyboard.press("o")
+    if request[0]==9:
+        keyboard.press("p")
+    if request[0]==10:
+        keyboard.press("a")
+    if request[0]==11:
+        keyboard.press("s")
+    if request[0]==12:
         keyboard.press("d")
+    if request[0]==13:
+        keyboard.press("f")
+    if request[0]==14:
+        keyboard.press("g")
+    if request[0]==15:
+        keyboard.press("h")
+    if request[0]==16:
+        keyboard.press("j")
+    if request[0]==17:
+        keyboard.press("k")
+    if request[0]==18:
+        keyboard.press("l")
+    if request[0]==19:
+        keyboard.press("z")
+    if request[0]==20:
+        keyboard.press("x")
+    if request[0]==21:
+        keyboard.press("c")
+    if request[0]==22:
+        keyboard.press("v")
+    if request[0]==23:
+        keyboard.press("b")
+    if request[0]==24:
+        keyboard.press("n")
+    if request[0]==25:
+        keyboard.press("m")
     print(str(request[0]))
         
     c.close()
